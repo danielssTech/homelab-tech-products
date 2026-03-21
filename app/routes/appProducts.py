16c57d6d762d49d62c94e81045fa534356afc2f5
danielssTech <danielss.tech195@gmail.com>
2026-03-21 10:04:21 +0000

    add new Alembic dependency for database migrations

--- app/routes/appProducts.py
+++ app/routes/appProducts.py
@@ -10,9 +10,6 @@ from app.services import product_service
 
 product_router = APIRouter (tags=["techProduct"])
 
-@product_router.post('/products', status_code=status.HTTP_201_CREATED)
-def create (request: product_schema.ProductCreate, db: Session = Depends(get_db) ): 
-    return product_service.post_product_service(request, db)
 
 @product_router.get('/products_get', response_model=list[product_schema.ProductOut])
 def list_items(skip: int = Query(0, ge=0),limit: int = Query(10, ge=1, le=100),
@@ -23,6 +20,9 @@ def list_items(skip: int = Query(0, ge=0),limit: int = Query(10, ge=1, le=100),
 def get_item(  id: int , db: Session = Depends(get_db)):
     return product_service.get_product_service(db, id)
 
+@product_router.post('/products', status_code=status.HTTP_201_CREATED)
+def create (request: product_schema.ProductCreate, db: Session = Depends(get_db) ): 
+    return product_service.post_product_service(request, db)
 
 @product_router.delete('/product/{id}')
 def delete_item (id: int, db: Session = Depends(get_db)):
